9e8a4159e17e3f4b4bae75f7fca2e764d766134a
flauzz <lucas.flauzino93@gmail.com>
2022-11-08 21:24:51 -0300

    primeiro commit

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,240 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel",
+        "sap/m/MessageBox",
+        "sap/m/MessageToast"
+    ], function (Controller, JSONModel, MessageBox, MessageToast) {
+        "use strict";
+        return Controller.extend("epm11.controller.Main", {    
+            onInit: function() {
+
+                this.inicializarModelosLocais();
+
+                //resgata o Roteador
+                let oRouter = this.getOwnerComponent().getRouter();
+
+                //resgata o Modelo
+                let oModel = this.getOwnerComponent().getModel();
+
+                //resgata a View
+                let oView = this.getView();
+                let oController = this;
+
+                //acessa a rota de detalhe, anexa o event patternMatched e declara função de callback para quando o evento for chamado
+                oRouter.getRoute("RotaDetalhe").attachPatternMatched(function(oEvent){
+
+                    //acessa o nome da rota
+                    let sRota = oEvent.getParameter("name");
+                    if(sRota === "RotaDetalhe"){
+                        oController.setarControlesEditaveis(false);
+                    }
+
+                    //acessa os argumentos do Evento
+                    let oArgumentos = oEvent.getParameter("arguments");
+
+                    //acessa o PartnerId
+                    let iPartnerId = oArgumentos.PartnerId;
+
+                    //gera o caminho do modelo
+                    let sCaminho = oModel.createKey("/BusinessPartnerSet", {
+                        PartnerId: iPartnerId
+                    });
+                    oView.bindElement(sCaminho);
+                });
+
+                oRouter.getRoute("RotaAdicionar").attachPatternMatched(function(oEvent){
+
+                    this.setarPropriedadeModelo("editavel", "/editavel", true);
+                    
+                    //habilitar o modo de edição
+                    this.setarPropriedadeModelo("botoes", "/editar", true);
+
+                    //desabilitar o modo de visualização
+                    this.setarPropriedadeModelo("botoes", "/visualizar", false);
+
+                    var oContext = oModel.createEntry("/BusinessPartnerSet", {
+                        properties: {
+                            PartnerId: "",
+                            PartnerType: "",
+                            PartnerName1: "",
+                            PartnerName2: "",
+                            SearchTerm1: "",
+                            SearchTerm2: "",
+                            Street: "",
+                            HouseNumber: "",
+                            District: "",
+                            City: "",
+                            Region: "",
+                            ZipCode: "",
+                            Country: ""
+                        }
+                    });
+                    oView.bindElement(oContext.getPath());
+                }.bind(this));
+            },
+
+            setarControlesEditaveis: function(bValor){
+               
+                var sPath ;
+                let oModel = new JSONModel();
+
+                //se verdadeiro
+                if(bValor){
+
+                    //carrega modelo a partir do arquivo controlesAbertos
+                    oModel.loadData('./model/controlesAbertos.json');
+                }
+                //se falso
+                else{
+
+                    //carrega modelo a partir do arquivo controlesFechados
+                    oModel.loadData('./model/controlesFechados.json');
+                   
+                }
+                this.getView().setModel(oModel, "editavel");
+            },
+
+            aoEditar: function(oEvent){
+                //versão anterior:
+                //resgatar o modelo
+                //let oModel = this.getView().getModel("editavel");
+
+                //altera o valor da propriedade "editavel"
+                //oModel.setProperty("/editavel", true);
+
+                //versão nova:
+                this.setarPropriedadeModelo("editavel", "/editavel", true);                
+                //habilitar o modo de edição
+                this.setarPropriedadeModelo("botoes", "/editar", true);
+                //desabilitar o modo de visualização
+                this.setarPropriedadeModelo("botoes", "/visualizar", false);
+
+                //grava o caminho do contexto do parceiro
+                this.sCaminhoContexto = this.getView().getBindingContext().getPath();
+            },
+
+            aoCancelar: function(oEvent){
+
+                MessageBox.show("Deseja cancelar a edição?", {
+                    title: "Cancelamento de edição",
+                    actions: [ MessageBox.Action.YES, MessageBox.Action.NO ],
+                    onClose: function(oAction){
+                        if(oAction === MessageBox.Action.YES){
+                            // //altera o valor da propriedade "editavel"
+                            // oModel.setProperty("/editavel", false);
+                            this.setarPropriedadeModelo("editavel", "/editavel", false);
+                            //habilitar o modo de edição
+                            this.setarPropriedadeModelo("botoes", "/editar", false);
+                            //desabilitar o modo de visualização
+                            this.setarPropriedadeModelo("botoes", "/visualizar", true);
+                            // //resgatar o modelo
+                            let oModel = this.getView().getModel();
+                            // cria array com um elemento dentro - o caminho do contexto
+                            let aCaminhos = [
+                                this.sCaminhoContexto
+                            ];                
+                            //reseta alterações
+                            oModel.resetChanges(aCaminhos);
+                        }
+                    }.bind(this) //salva o contexto do this apontando para o controller fora da função
+                });
+           },
+
+           aoSalvar: function(oEvent) {
+                //resgata o Router
+                let oRouter = this.getOwnerComponent().getRouter();
+
+                //resgata o modelo
+                var oModel = this.getView().getModel();
+
+                //resgata o form
+                var oForm = this.getView().byId("FormParceiro");
+
+                //adiciona o ícone de carregamento
+                oForm.setBusy(true);
+
+                var oDados = this.getView().getBindingContext().getObject();
+
+                if(oDados.PartnerId) {
+
+                    oModel.update(this.sCaminhoContexto, oDados, {
+                        success: function (oData){
+                            MessageToast.show("Atualização do parceiro " + oDados.PartnerId + " feita com sucesso!");
+                            oForm.setBusy(false);
+                            // //altera o valor da propriedade "editavel"
+                            // oModel.setProperty("/editavel", false);
+                            this.setarPropriedadeModelo("editavel", "/editavel", false);
+                            //habilitar o modo de edição
+                            this.setarPropriedadeModelo("botoes", "/editar", false);
+                            //desabilitar o modo de visualização
+                            this.setarPropriedadeModelo("botoes", "/visualizar", true);
+                        }.bind(this),
+                        error: function (oError){
+                            MessageBox.error("Erro ao atualizar parceiro " + oDados.PartnerId);
+                            oForm.setBusy(false);
+                        }
+                    });
+
+                } else {
+                    oModel.create("/BusinessPartnerSet", oDados, {
+                        success: function(oData) {
+                            MessageToast.show("Parceiro " + oData.PartnerId + " criado!");
+
+                            //altera o valor da propriedade "editavel"
+                            this.setarPropriedadeModelo("editavel", "/editavel", false);
+                            //habilitar o modo de edição
+                            this.setarPropriedadeModelo("botoes", "/editar", false);
+                            //desabilitar o modo de visualização
+                            this.setarPropriedadeModelo("botoes", "/visualizar", true);
+
+                            oForm.setBusy(false);
+
+                            oRouter.navTo("RouteView1", true);
+                        }.bind(this),
+                        error: function(oError) {
+                            var oErro = JSON.parse(oError.responseText);
+                            MessageBox.error("Erro ao criar: " + oErro.error.message.value);
+                            oForm.setBusy(false);
+                        }
+                    })
+                }
+           },
+
+            aoDigitarCEP: function(oEvent){
+
+                //acessa o valor do Input
+                let sValor = oEvent.getParameter("value");
+
+                //resgata o Input
+                var oInput = oEvent.getSource();
+
+                if(sValor.length !== 9){
+                    oInput.setValueState(sap.ui.core.ValueState.Error);
+                } else {
+                    oInput.setValueState(sap.ui.core.ValueState.None);
+                }
+            },
+
+            inicializarModelosLocais: function() {
+
+                //cria nova instância de um modelo JSON
+                var oModeloBotao = new JSONModel();
+                //criar propriedade visualizar que controla o modo de visualização
+                oModeloBotao.setProperty("/visualizar", true);
+                //criar propriedade editar que controla o modo de edição
+                oModeloBotao.setProperty("/editar", false);
+
+                var oView = this.getView();
+                oView.setModel(oModeloBotao, "botoes");
+            },
+
+            setarPropriedadeModelo: function(sModelo, sPropriedade, bValor) {
+                var oModel = this.getView().getModel(sModelo);
+                //validar que a instância não é undefined
+                if(oModel) {
+                    oModel.setProperty(sPropriedade, bValor);
+                }
+            }
+        });
+    });
---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,32 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ], function (Controller) {
+        "use strict";
+        return Controller.extend("epm11.controller.Main", {  
+
+            onInit: function(){
+            },
+
+            aoPressionarItem: function(oEvent){
+                //resgatar a coluna clicada
+                let oColumnListItem = oEvent.getSource();
+                // resgata o binding da coluna clicada. O Modelo não tem nome, então o getBindingContext fica sem parâmetro
+                let oItem = oColumnListItem.getBindingContext().getObject();
+                // resgata o ID do parceiro
+                let iPartnerId = oItem.PartnerId;
+                //resgata o Roteador
+                let oRouter = this.getOwnerComponent().getRouter();
+                //navega p/ rota de detalhe
+                oRouter.navTo("RotaDetalhe", {
+                    PartnerId: iPartnerId
+                });
+            },
+
+            aoAdicionar: function(oEvent){
+                let oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("RotaAdicionar");
+            }
+            
+            });
+        });
